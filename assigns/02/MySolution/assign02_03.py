d5b3866142f171ed6b6b16a805bafd1ab5da20e1
prianna sharan <psharan@bu.edu>
2023-02-09 17:43:15 -0500

    can't figure out error

--- assigns/02/MySolution/assign02_03.py
+++ assigns/02/MySolution/assign02_03.py
@@ -20,3 +20,25 @@ def split(xs):
         else:
             (ys, zs) = split(xs.get_cons2().get_cons2())
             return (mylist_cons(xs.get_cons1(), ys), mylist_cons(xs.get_cons2().get_cons1(), zs))
+
+def merge(ys, zs):
+    if mylist_nilq(ys):
+        return zs
+    else:
+        if mylist_nilq(zs):
+            return mylist_cons(ys.get_cons1(), ys.get_cons2())
+        else:
+            if ys.get_cons1() <= zs.get_cons1():
+                return mylist_cons(ys.get_cons1(), merge(ys.get_cons2(), mylist_cons(zs.get_cons1(), zs.get_cons2())))
+            else:
+                return mylist_cons(zs.get_cons1(), merge(mylist_cons(ys.get_cons1(), ys.get_cons2()), zs.get_cons2()))
+
+def mylist_mergesort(xs):
+	if mylist_nilq(xs):
+		return mylist_nil()
+	else:
+		if mylist_nilq(xs.get_cons2()):
+			return mylist_cons(xs.get_cons2(), mylist_nil())
+		else:
+			(ys, zs) = split(xs.get_cons2()
+			return merge(mylist_mergesort(mylist_cons(xs.get_cons1(), ys)), mylist_mergesort(mylist_cons(xs.get_cons2().get_cons1(), zs)))
