fd40d8e64dfc9db4d4763c707693acb6b5e21ced
prianna sharan <psharan@bu.edu>
2023-02-09 18:26:02 -0500

    finalized

--- assigns/02/MySolution/assign02_03.py
+++ assigns/02/MySolution/assign02_03.py
@@ -14,12 +14,11 @@ print("[import ./../assign02.py] is done!")
 def split(xs):
     if mylist_nilq(xs):
         return (mylist_nil(), mylist_nil())
+    elif mylist_nilq(xs.get_cons2()):
+        return (mylist_cons(xs.get_cons1(), mylist_nil()), mylist_nil())
     else:
-        if mylist_nilq(xs.get_cons2()):
-            return (mylist_cons(xs.get_cons1(), mylist_nil()), mylist_nil())
-        else:
-            (ys, zs) = split(xs.get_cons2().get_cons2())
-            return (mylist_cons(xs.get_cons1(), ys), mylist_cons(xs.get_cons2().get_cons1(), zs))
+        (ys, zs) = split(xs.get_cons2().get_cons2())
+        return (mylist_cons(xs.get_cons1(), ys), mylist_cons(xs.get_cons2().get_cons1(), zs))
 
 def merge(ys, zs):
     if mylist_nilq(ys):
@@ -27,17 +26,16 @@ def merge(ys, zs):
     elif mylist_nilq(zs):
         return ys
     else:
-        if ys.get_cons1() <= zs.get_cons1():
-            return mylist_cons(ys.get_cons1(), merge(ys.get_cons2(), zs))
+        if ys.get_cons1()<=zs.get_cons1():
+            return mylist_cons(ys.get_cons1(),merge(ys.get_cons2(),zs))
         else:
-            return mylist_cons(zs.get_cons1(), merge(zs.get_cons2(), ys))
+            return mylist_cons(zs.get_cons1(), merge(zs.get_cons2(),ys))
 
 def mylist_mergesort(xs):
 	if mylist_nilq(xs):
 		return mylist_nil()
+	elif mylist_nilq(xs.get_cons2()):
+		return mylist_cons(xs.get_cons1(), mylist_nil())
 	else:
-		if mylist_nilq(xs.get_cons2()):
-			return mylist_cons(xs.get_cons2(), mylist_nil())
-		else:
-			(ys, zs) = split(xs.get_cons2().get_cons2())
-			return merge(mylist_mergesort(mylist_cons(xs.get_cons1(), ys)), mylist_mergesort(mylist_cons(xs.get_cons2().get_cons1(), zs)))
+		(ys, zs) = split(xs.get_cons2().get_cons2())
+		return merge(mylist_mergesort(mylist_cons(xs.get_cons1(), ys)), mylist_mergesort(mylist_cons(xs.get_cons2().get_cons1(), zs)))
