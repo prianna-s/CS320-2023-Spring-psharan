53bfdf305e6baf02fca0395bca4059354d294950
prianna sharan <psharan@bu.edu>
2023-02-09 13:34:14 -0500

    finished problem 1

--- assigns/02/MySolution/assign02_01.py
+++ assigns/02/MySolution/assign02_01.py
@@ -12,3 +12,19 @@ print("[import ./../assign02.py] is done!")
 # mylist_reverse (see list_reverse in assign02.sml)
 #
 ####################################################
+
+def mylist_append(xs, ys):
+	if mylist_nilq(xs)::
+		return ys
+	else:
+		return mylist_cons(xs.cons1, mylist_append(xs.cons2, ys))
+    
+def mylist_rappend(xs, ys):
+	if mylist_nilq(xs):
+		return ys
+	else:
+		return mylist_rappend(xs.cons2, mylist_cons(xs.cons1, ys))
+
+def mylist_reverse(xs):
+	return mylist_rappend(xs, mylist_nil())
+
