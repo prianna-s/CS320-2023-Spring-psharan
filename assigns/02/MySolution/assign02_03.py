9d9184a33e0eb93f9f629c826a100337ad228412
prianna sharan <psharan@bu.edu>
2023-02-09 16:25:35 -0500

    finished split

--- assigns/02/MySolution/assign02_03.py
+++ assigns/02/MySolution/assign02_03.py
@@ -10,3 +10,13 @@ print("[import ./../assign02.py] is done!")
 # mylist_mergesort (see list_mergesort in assign02.sml)
 #
 ####################################################
+
+def split(xs):
+    if mylist_nilq(xs):
+        return (mylist_nil(), mylist_nil())
+    else:
+        if mylist_nilq(xs.get_cons2()):
+            return (mylist_cons(xs.get_cons1(), mylist_nil()), mylist_nil())
+        else:
+            (ys, zs) = split(xs.get_cons2().get_cons2())
+            return (mylist_cons(xs.get_cons1(), ys), mylist_cons(xs.get_cons2().get_cons1(), zs))
